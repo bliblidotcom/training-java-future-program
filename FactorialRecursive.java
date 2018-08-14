692a6dc504b61f466abcd187623c23a1251cbd18
Jonathan <jonathan16998@gmail.com>
2018-08-14 10:56:39 +0700

    Created FactorialRecursive.java file
    Added empty constructor and main method with try-catch for handling
    empty argument call.

---
+++ FactorialRecursive.java
@@ -0,0 +1,13 @@
+public class FactorialRecursive {
+	public FactorialRecursive(){
+
+	}
+
+	public static void main(String[] args){
+		try{
+			new FactorialRecursive();
+		} catch (Exception e){
+			System.out.println("Empty argument!");
+		}
+	}
+}
