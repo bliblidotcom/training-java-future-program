f08eae74652f112d0f2161fd367894805039273e
Jonathan <jonathan16998@gmail.com>
2018-08-14 11:02:22 +0700

    Finished working with FactorialRecursive.java
    Finished implementing factorial calculation using recursive method.

--- FactorialRecursive.java
+++ FactorialRecursive.java
@@ -1,11 +1,21 @@
 public class FactorialRecursive {
-	public FactorialRecursive(){
+	private Integer recursive(Integer number){
+		if(number > 1){
+			return number * recursive(number - 1);
+		} else {
+			return 1;
+		}
+	}
 
+	public FactorialRecursive(Integer number){
+		Integer result = recursive(number);
+
+		System.out.println(result);
 	}
 
 	public static void main(String[] args){
 		try{
-			new FactorialRecursive();
+			new FactorialRecursive(Integer.parseInt(args[0]));
 		} catch (Exception e){
 			System.out.println("Empty argument!");
 		}
