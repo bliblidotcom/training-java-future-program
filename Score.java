064a488e3598f806ec25a49d610fb7d63482c948
cacadosman <wetmanz23@gmail.com>
2018-08-14 13:25:22 +0700

    JAVA-01 Scoring Nilai
    Pelatihan java tentang conditional if-else

---
+++ Score.java
@@ -0,0 +1,22 @@
+public class Score{
+    public static void main(String[] args){
+        if(args.length != 1){
+            System.out.println("Usage: java Score <score>");
+            return;
+        }
+        
+        int score = Integer.parseInt(args[0]);
+
+        if(score <= 20){
+            System.out.println("E");
+        }else if(score > 20 && score <= 40){
+            System.out.println("D");
+        }else if(score > 40 && score <= 60){
+            System.out.println("C");
+        }else if(score > 60 && score <= 80){
+            System.out.println("B");
+        }else{
+            System.out.println("A");
+        }
+    }
+}
